12fd2e0f462db99f1bb98179af2dda1e551aebb9
Nikita Reshetov <nikita.reshetov@office.e2e4gu.ru>
2024-04-14 14:59:08 +0700

    new build version 1.0.0.1(2) - fixed audio visualizer request permission issue. Compatible with Android 8.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.niresh23.fanlightcontroller"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0.0"
+        versionCode = 2
+        versionName = "1.0.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
