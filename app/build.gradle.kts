25c8d7c19ae699b98e2a926698060e1bf1e84471
Nikita Reshetov <niksama3@gmail.com>
2026-07-05 16:22:55 +0700

    up version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.niresh23.fanlightcontroller"
         minSdk = 26
         targetSdk = 36
-        versionCode = 12
+        versionCode = 13
         versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
